36f7b5e5549f97b2ea8a8bec42df32d3d71bda90
huangkewei123 <huangkewei>
2019-09-04 00:29:24 +0800

    删除无用代码

--- src/main/java/com/example/demo/jee/utils/email/SimpleMailSender.java
+++ src/main/java/com/example/demo/jee/utils/email/SimpleMailSender.java
@@ -3,28 +3,22 @@ package com.example.demo.jee.utils.email;
 /**
  * 简单邮件（不带附件的邮件）发送器
  */
-import java.io.File;
-import java.util.*;
-import javax.activation.DataHandler;
-import javax.activation.FileDataSource;
-import javax.mail.Address;
-import javax.mail.BodyPart;
-import javax.mail.Message;
-import javax.mail.Message.RecipientType;
-import javax.mail.Multipart;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeBodyPart;
-import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMultipart;
-import javax.mail.internet.MimeUtility;
 
 import com.alibaba.fastjson.JSONObject;
 import com.example.demo.jee.constants.SysConstants;
-import com.example.demo.jee.utils.SpiderUtile.SpiderXpathUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javax.activation.DataHandler;
+import javax.activation.FileDataSource;
+import javax.mail.*;
+import javax.mail.Message.RecipientType;
+import javax.mail.internet.*;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Properties;
 
 /**
  * 简单邮件（不带附件的邮件）发送器
